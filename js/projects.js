2e68fad7f4763ca1068b0a866e0273e560d4f886
Vyshak Srishylappa <vsrishylappa@agero.com>
2018-12-24 23:12:23 -0500

    Inital Commit.

---
+++ js/projects.js
@@ -0,0 +1,16 @@
+oss_projects = {};
+oss_projects['di-container'] = {
+  blog_post: 'https://github.com/agero-core/di-container/blob/master/README.md',
+  featured: true,
+  position: 1
+};
+oss_projects['lazy'] = {
+  blog_post: 'https://github.com/agero-core/lazy/blob/master/README.md',
+  featured: true,
+  position: 2
+};
+oss_projects['splunk-logger'] = {
+  blog_post: 'https://github.com/agero-core/lazy/splunk-logger/master/README.md',
+  featured: true,
+  position: 3
+};
