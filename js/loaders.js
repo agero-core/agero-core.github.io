43eed574efcb07a68c1125e393a8e229897c0728
Vyshak Srishylappa <vyshaksrishyla@gmail.com>
2019-01-02 13:30:25 -0500

    Code: Remove not needed coded.

--- js/loaders.js
+++ js/loaders.js
@@ -1,30 +1,3 @@
-function getAllPages(urlPrefix, callback, page, results) {
-  page = page || 1;
-  results = results || [];
-
-  var url = urlPrefix + '?per_page=100&page=' + parseInt(page);
-
-  $.get(url, function(data) {
-    if (data.length > 0) {
-      data.forEach(function(resultDatum) {
-        results.push(resultDatum);
-      });
-      getAllPages(urlPrefix, callback, page + 1, results);
-    }
-    else {
-      callback(results);
-    }
-  });
-}
-
-function getGithubRepos(callback, page, repos) {
-  getAllPages('https://api.github.com/users/agero-core/repos', callback);
-}
-
-function getGithubMembers(callback) {
-  getAllPages('https://api.github.com/orgs/agero-core/members', callback);
-}
-
 function loadRepositoryData(repoData) {
   var org = new Organization('agero-core');
   org.repos = [];
@@ -42,11 +15,5 @@ function loadRepositoryData(repoData) {
 function loadMemberData(members) {
   $('.dev-count').html(members.length);
 }
-
-$(document).ready(function() {
-  //getGithubRepos(loadRepositoryData);
-  //getGithubMembers(loadMemberData);
-});
-
 
 // vim: sw=2 sts=2 expandtab
