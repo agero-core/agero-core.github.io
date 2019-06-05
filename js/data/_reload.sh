7d5fb7c444e8fa22d5e63c596b6acee2ed491f00
Andrey Sergeev <andrey@chyuck.com>
2019-06-05 10:06:06 -0400

    Updated _reload.sh script and repos

--- js/data/_reload.sh
+++ js/data/_reload.sh
@@ -3,33 +3,13 @@
 cd $(dirname $0)
 
 get_all_pages() {
-  local url_prefix="$1"
+  local url="$1"
   local outfile=$(mktemp)
-  local response=
-  local page_size=100
-  local page=1
-
-  while [ "$response" != "[]" ]
-  do
-    local url="$url_prefix?per_page=$page_size&page=$page"
-    echo "$url" >&2
-
-    response=$(curl -sf "$url" | jq . | tee -a "$outfile")
-
-    if [ $? -ne 0 ]
-    then
-      echo "curl failed, so I'm gonna get out of here." >&2
-      echo "[]"
-      exit 1
-    fi
-
-    let page=page+1
-  done
+  local response=$(curl -sf "$url" | jq . | tee -a "$outfile")
+  echo "$url" >&2
 
   cat "$outfile" | jq '.[]' | jq -s .
 }
-
-# Repos
 
 get_repos() {
   get_all_pages 'https://api.github.com/users/agero-core/repos'
